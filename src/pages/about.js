dfe5a6b90a178538de5e4773b44143fe170443ab
Jonne Krom <jerotozeros@gmail.com>
2022-02-08 16:12:53 +0100

    Finished Gatsby Tutorial Part 3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,16 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 // Step 2: Define your component
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
     <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    <StaticImage
+      alt="Me figuring out the cables"
+      src="../images/lottacables.jpg"
+    />
   </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     
        <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="What is happening with my server room"
+        src="https://pbs.twimg.com/media/FB7KWTkVgAAX7sG?format=jpg&name=4096x4096"
+      />
     </Layout>
     
   )
